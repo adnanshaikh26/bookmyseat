25c2271e2b6058da5bc75ea58e63757a90bf115a
adnanshaikh26 <adnan.shk.26@gmail.com>
2025-10-30 20:45:00 +0530

    feat(booking):"implemented booking ui"

--- booking-app/src/components/Booking.jsx
+++ booking-app/src/components/Booking.jsx
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { seats } from "../utils/constant";
 import { Armchair } from "phosphor-react";
 
 export default function Booking() {
+  const [currentSeats, setCurrentSeats] = useState(seats);
+  const selectHandler = (seat) => {
+    console.log("Selected Seat:", seat.row + seat.number);
+    if (seat.isBooked) return;
+    setCurrentSeats((prevSeats) =>
+      prevSeats.map((s) =>
+        s.id === seat.id ? { ...s, isSelected: !s.isSelected } : s
+      )
+    );
+  };
   return (
     <section className="max-w-3xl mx-auto p-4 border border-gray-700 rounded-lg mt-10">
       <h1 className="text-2xl font-bold mb-4">Book Your Seat</h1>
-      <section className="flex">
+      <section className="flex gap-8">
         <div className="grid grid-cols-1 gap-2 ">
-          {["A", "B"].map((row) => (
-            <div key={row} className="flex items-center justify-center h-8 w-8">
+          {["A", "B", "C", "D", "E"].map((row) => (
+            <div
+              key={row}
+              className="flex items-center justify-center h-8 w-8 text-gray-500 text-sm"
+            >
               {row}
             </div>
           ))}
         </div>
         <div className="grid grid-cols-8 gap-2 w-full">
-          {seats.map((seat) => (
-            <button key={seat.id}>
+          {currentSeats.map((seat) => (
+            <button
+              key={seat.id}
+              className={`cursor-pointer hover:bg-[#ffffff7d] ${
+                seat.isSelected ? "bg-[#ffffff3d]" : ""
+              }`}
+              onClick={() => selectHandler(seat)}
+            >
               <div className="flex justify-center">
-                <Armchair size={32} />
+                {seat.isBooked ? (
+                  <Armchair size={32} className="text-red-800" />
+                ) : (
+                  <Armchair size={32} />
+                )}
               </div>
             </button>
           ))}
         </div>
       </section>
+      <div className="w-full flex justify-center mt-24">
+        <h2 className="text-xs font-semibold  w-[550px] bg-blue-100 text-blue-500 rounded">
+          SCREEN
+        </h2>
+      </div>
     </section>
   );
 }
